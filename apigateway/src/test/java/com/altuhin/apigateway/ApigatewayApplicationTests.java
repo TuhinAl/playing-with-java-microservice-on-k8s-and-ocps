a76d6b85a591652b4a4ab903cf92116f3a4bd8d8
alauddintuhin1411@gmail.com <alauddintuhin1411@gmail.com>
2022-06-17 12:09:48 +0600

    added api gateway service

---
+++ apigateway/src/test/java/com/altuhin/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altuhin.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
