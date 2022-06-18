796f31c2432de5153ad2d9d61199598d74cdd352
alauddintuhin1411@gmail.com <alauddintuhin1411@gmail.com>
2022-06-18 12:42:52 +0600

    added jpa association service

---
+++ jpaassociation/src/main/java/com/altuhin/jpaassociation/JpaassociationApplication.java
@@ -0,0 +1,14 @@
+package com.altuhin.jpaassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaassociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaassociationApplication.class, args);
+		System.out.println("***Application Running****");
+	}
+
+}
---
+++ jpaassociation/src/test/java/com/altuhin/jpaassociation/JpaassociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altuhin.jpaassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaassociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
